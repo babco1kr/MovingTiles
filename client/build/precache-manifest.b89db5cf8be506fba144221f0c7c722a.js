b7be3658b7e7fff1937e7ffebe220c6bbcf12b72
Kyle <kylerbabcock@gmail.com>
2019-05-19 12:08:16 -0400

    getting ready to deploy first build to heroku

---
+++ client/build/precache-manifest.b89db5cf8be506fba144221f0c7c722a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50799b5a6f851c3d1c2da5677c44f054",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c339edf973c355f94fd8",
+    "url": "/static/css/main.854edcbf.chunk.css"
+  },
+  {
+    "revision": "cfbd4a7b9444cc428efb",
+    "url": "/static/js/2.5ea3732e.chunk.js"
+  },
+  {
+    "revision": "c339edf973c355f94fd8",
+    "url": "/static/js/main.c83dd70e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
